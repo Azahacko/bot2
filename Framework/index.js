f4b4bc80021a639799a9c91f521db18d502bfc18
THOMAS  SECONDE  DEVIS <kouamedjakiss766@gmail.com>
2025-02-25 17:59:50 +0000

    up

--- Framework/index.js
+++ Framework/index.js
@@ -1,1 +1,10 @@
+const nomCmds = 'other-command';
+const desc = 'Description de la commande autre.';
+const alias = ['!autre-commande'];
+
+const execute = async (client, message, args) => {
+    await message.reply('Voici une réponse à votre commande autre.');
+};
+
+module.exports = { nomCmds, desc, alias, execute };
 
