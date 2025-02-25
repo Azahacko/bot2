5fa5953c2efa911cbdd60adf8c15feaf615f0e3e
Azahacko <azahacksaconception@gmail.com>
2025-02-25 18:46:13 +0000

    Update and rename rules.json to rules.js

---
+++ rules.js
@@ -0,0 +1,20 @@
+const reglement = `
+🌎 RÈGLEMENT INTÉRIEUR 🌍SPIDER-NET🌍™
+
+PRÉROGATIVES DES ADMINISTRATEURS
+
+Article 1️⃣: Seul le CONSEIL D'ADMINISTRATION peut ajouter des membres.
+Article 2️⃣: Seul le CONSEIL D'ADMINISTRATION peut nommer/révoquer des administrateurs.
+Article 3️⃣: Un administrateur ne peut révoquer un autre administrateur. Une sanction peut être votée en cas d'infraction grave (absentéisme, abus d'autorité, etc.).
+Article 4️⃣: Tous les administrateurs sont égaux et doivent respecter l'éthique.
+Article 5️⃣: Un administrateur doit encadrer les membres et peut supprimer un membre perturbateur.
+Article 6️⃣: La cour de justice de la communauté gère les litiges des membres non administrateurs.
+Article 7️⃣: Les co-fondateurs ont un statut spécial et une immunité administrative.
+Article 8️⃣: Le CONSEIL D'ADMINISTRATION et le Conseil des CO-FONDATEURS sont complémentaires.
+Article 9️⃣: Le CONSEIL D'ADMINISTRATION gère tous les groupes sauf celui des CO-FONDATEURS.
+Article 🔟: Seuls certains membres qualifiés peuvent programmer des réunions et prendre des décisions importantes.
+
+⚠️ Nul n'est au-dessus du règlement intérieur, pas même le CONSEIL D'ADMINISTRATION, le Conseil des CO-FONDATEURS ou la JUSTICE de la communauté.
+`;
+
+module.exports = reglement;
